8430124ceb902b7f66fe347740999074018e092e
cwl0429 <chunweilai29@gmail.com>
2022-11-14 19:53:23 +0800

    Remove used methods and members

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import pickle
 import numpy as np
 import argparse    
 import os
-from processing import get_single_data, calculate_position
+from processing import Processing
 import torch
 from visualize import AnimePlot
 
@@ -12,40 +12,36 @@ from visualize import AnimePlot
 
 class Inference:
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-   
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--type", type=str, help="Evaluation type", required=True) # infilling/inter/smooth
-    parser.add_argument("-m", "--model", type=str, help="Model name", required=True) # e.g. Human3.6M_train_angle_01_1_1010
     parser.add_argument("-d", "--dataset", type=str, help="Dataset Dir", required=True) # e.g. Human3.6M/test_angle
     parser.add_argument("-f", "--file", type=str, help="File name")                          
-    parser.add_argument("-o", "--out", type=str, help="Output Dir", default='result/demo.pkl')
     parser.add_argument("-v", "--visual", help="Visualize", action="store_true")
-    parser.add_argument("-p","--partial", help = "Partial model", action="store_true")
     args = parser.parse_args()
-
+    
     partList = ['leftarm', 'rightarm', 'leftleg', 'rightleg', 'torso']
     dataset = args.dataset.split('/')[0]
     dir = args.dataset.split('/')[1]
     file = args.file
-    inp_len = int(args.model.split('_')[-1][:2])
-    out_len = int(args.model.split('_')[-1][-2:])
-    batch_size = 128
-    # stage  = 5
+    args_model = '1011_ChoreoMaster_Normal_train_angle_01_2010'
+    inp_len = int(args_model.split('_')[-1][:2])
+    out_len = int(args_model.split('_')[-1][-2:])
+    args_type = 'infilling'
+    
+
     with open("./data/TPose/s_01_act_02_subact_01_ca_01.pickle", 'rb')as fpick:
         TPose = pickle.load(fpick)[0]
 
+    def __init__(self):
+        self.models = {}
+        for part in self.partList:
+            self.models[part] = self.load_model(part)
+
     def load_model(self, part):
         # print(">>> Model loaded -->", part)
-        path = os.path.join("ckpt", self.args.model, part)
+        path = os.path.join("model", self.args_model, part)
         model = torch.load(path + "/best.pth",map_location = self.DEVICE).to(self.DEVICE)
         model.eval()
         return model
-
-    def load_data(self, file):
-        print(">>> Data loaded -->", file)
-        data = get_single_data(self.dataset, dir, file)
-        data = torch.tensor(data.astype("float32"))
-        return data
 
     def infilling(self, dim, model, data):
         motion = data.to(self.DEVICE)
@@ -72,7 +68,7 @@ class Inference:
             dim = 45
         else:
             dim = 18
-        if self.args.type == 'infilling':
+        if self.args_type == 'infilling':
             result = self.infilling(dim, model, data)
         else:
             print('No this type!!')
@@ -87,33 +83,26 @@ class Inference:
         result = np.concatenate((torso[:, 0:9], rarm[:, -6:], torso[:, 9:12], larm[:, -6:], torso[:, 12:18], rleg[:, -6:], torso[:, 18:21], lleg[:, -6:]), 1)
         return result
 
-    def main(self):
-        data = self.load_data(self.file)
-        if self.args.partial:
-            partDatas = {}
-            for part in self.partList:
-                model = self.load_model(part)
-                if part == 'torso':
-                    part_data = torch.cat((data[:, 0:9], data[:, 15:18], data[:, 24:30], data[:, 36:39]), axis=1)
-                elif part == 'leftarm':
-                    part_data = torch.cat((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 18:24]), axis=1)
-                elif part =='rightarm':
-                    part_data = torch.cat((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 9:15]), axis=1)
-                elif part == 'leftleg':
-                    part_data = torch.cat((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 39:45]), axis=1)
-                elif part == 'rightleg':
-                    part_data = torch.cat((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 30:36]), axis=1)
-
-                partDatas[part] = self.getResult(part_data, model, part)
-            pred = self.combine(partDatas)
-        else:
-            part = 'entire'
+    def main(self, data):
+        partDatas = {}
+        for part in self.partList:
             model = self.load_model(part)
-            pred = self.getResult(data, model, part)
-        pred = calculate_position(pred, self.TPose)
-        # gt = data
-        gt = calculate_position(data, self.TPose)
-        if self.args.type == 'infilling':
+            if part == 'torso':
+                part_data = torch.cat((data[:, 0:9], data[:, 15:18], data[:, 24:30], data[:, 36:39]), axis=1)
+            elif part == 'leftarm':
+                part_data = torch.cat((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 18:24]), axis=1)
+            elif part =='rightarm':
+                part_data = torch.cat((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 9:15]), axis=1)
+            elif part == 'leftleg':
+                part_data = torch.cat((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 39:45]), axis=1)
+            elif part == 'rightleg':
+                part_data = torch.cat((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 30:36]), axis=1)
+            partDatas[part] = self.getResult(part_data, model, part)
+        pred = self.combine(partDatas)
+        
+        pred = Processing.calculate_position(pred, self.TPose)
+        gt = Processing.calculate_position(data, self.TPose)
+        if self.args_type == 'infilling':
             result = np.zeros_like(pred)
             ran = int(self.inp_len/2)
             for j in range(0, len(gt)-ran+1, ran):
@@ -121,7 +110,11 @@ class Inference:
                 result[(ran + self.out_len) * step: (ran + self.out_len) * step + ran] = gt[j: j + ran]
             gt = result
         return gt, pred
-
+    '''
+    output .pkl and .gif
+    '''
+    def output(self):
+        pass
     # if __name__ == '__main__':
     #     gt, pred = main()
     #     path = args.out.split('.')
