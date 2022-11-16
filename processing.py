fc00ce41df6c9beaa922b90f0039c6941c65b526
cwl0429 <chunweilai29@gmail.com>
2022-11-16 20:26:51 +0800

    Move joints definition outside the class
    Allow other files to use joints definition without creating creating
    objects.

--- processing.py
+++ processing.py
@@ -2,23 +2,23 @@ import os
 import numpy as np
 import pickle
 
-class Processing:
-    def __init__(self) -> None:
-        self.jointIndex = {"head":0, "neck":3, "rshoulder":6, "rarm":9, "rhand":12, 
+jointIndex = {"head":0, "neck":3, "rshoulder":6, "rarm":9, "rhand":12, 
                 "lshoulder":15, "larm":18, "lhand":21, "pelvis":24, 
                 "rthigh":27, "rknee":30,"rankle":33,"lthigh":36, "lknee":39, "lankle":42}
 
-        self.joint = {"head":0, "neck":1, "rshoulder":2, "rarm":3, "rhand":4, 
-                        "lshoulder":5, "larm":6, "lhand":7, "pelvis":8, 
-                        "rthigh":9, "rknee":10,"rankle":11,"lthigh":12, "lknee":13, "lankle":14}
+joint = {"head":0, "neck":1, "rshoulder":2, "rarm":3, "rhand":4, 
+            "lshoulder":5, "larm":6, "lhand":7, "pelvis":8, "rthigh":9, 
+            "rknee":10,"rankle":11,"lthigh":12, "lknee":13, "lankle":14}
 
-        self.jointChain = [["neck","pelvis"], ["head","neck"],  ["rshoulder", "neck"], ["rarm", "rshoulder"], ["rhand", "rarm"],
-                        ["rthigh", "pelvis"], ["rknee", "rthigh"], ["rankle", "rknee"],
-                        ["lshoulder", "neck"], ["larm", "lshoulder"], ["lhand", "larm"], 
-                        ["lthigh", "pelvis"], ["lknee", "lthigh"], ["lankle", "lknee"]]
-    
-        self.jointConnect = [(self.jointIndex[joint[0]], self.jointIndex[joint[1]]) for joint in self.jointChain]
+jointChain = [["neck","pelvis"], ["head","neck"],  ["rshoulder", "neck"], ["rarm", "rshoulder"], 
+                ["rhand", "rarm"],["rthigh", "pelvis"], ["rknee", "rthigh"], ["rankle", "rknee"],
+                ["lshoulder", "neck"], ["larm", "lshoulder"], ["lhand", "larm"], 
+                ["lthigh", "pelvis"], ["lknee", "lthigh"], ["lankle", "lknee"]]
 
+jointConnect = [(jointIndex[joint[0]], jointIndex[joint[1]]) for joint in jointChain]
+
+
+class Processing:
     def get_single_data(self, dir, filename, file):
         if dir != "":
             filepath = os.path.join("../Dataset", dir, filename, file)
