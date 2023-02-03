40169b88bb0be7a9d0a2d98cac1a86fff4b65c40
cwl0429 <chunweilai29@gmail.com>
2023-02-03 12:34:33 +0800

    Update input(.xlsx) format

--- utils.py
+++ utils.py
@@ -16,22 +16,26 @@ class Utils:
     def combine_selected_motion(self):
         is_first_data = True
         motion_len = []
+        interpo_len = []
         for i, row in enumerate(self.ws.iter_rows(values_only=True, min_row=2)):
-            for anim in row:
-                if anim is not None:
-                    data = np.load(self.dataset_path + anim)
-                    if is_first_data != True:
-                        motion = np.concatenate((motion,data))
-                    else:
-                        motion = data
-                        is_first_data = False
-                    motion_len.append(len(data))
+            anim = row[0]
+            interpo_len.append(row[1])
+            if anim is not None:
+                data = np.load(self.dataset_path + anim)
+                if is_first_data != True:
+                    motion = np.concatenate((motion,data))
+                else:
+                    motion = data
+                    is_first_data = False
+                motion_len.append(len(data))
+
+        assert len(motion_len) >= 2
         motion = self.processing.normalize(motion)
         motion = self.processing.calculate_angle(motion)
         return motion, motion_len
     
 if __name__ == "__main__":
-    file_name = "excel_input_test.xlsx"
+    file_name = "template_frames_160_bpm_90.xlsx"
     utils = Utils(file_name)
     utils.load_xlsx()
     data = utils.combine_selected_motion()
